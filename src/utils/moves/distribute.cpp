00698b2ff0bfba280500be0ba460530c6de5d332
Utkarsh Naman <truly.away@gmail.com>
2025-12-23 00:51:35 +0530

    added distribution of fingers

--- src/utils/moves/distribute.cpp
+++ src/utils/moves/distribute.cpp
@@ -4,14 +4,36 @@
 
 #include "../../../include/utils/moves/distribute.hpp"
 
+#include <algorithm>
+
 #include "../../../include/rules.hpp"
 #include "../../../include/utils/state_repr.hpp"
+
+static vector<Player> distribution_pairs(Player fingers)
+{
+    vector<tuple<short, short>> distribution_pairs_list;
+    distribution_pairs_list.push_back(fingers);
+    //some code here
+    auto sum = static_cast<short>(get<0>(fingers)+get<1>(fingers));
+    for (short i = 0; i < MAX_FINGER; i++)
+    {
+        Player sol_i;
+        get<0>(sol_i) = min(i, static_cast<short>((sum-i)%MAX_FINGER));
+        get<1>(sol_i) = max(i, static_cast<short>((sum-i)%MAX_FINGER));
+        auto it = find(distribution_pairs_list.begin(), distribution_pairs_list.end(), sol_i);
+        if (it == distribution_pairs_list.end()) distribution_pairs_list.push_back(sol_i);
+    }
+
+    return distribution_pairs_list;
+}
+
 
 Next_State_str_vector distribute(State_tuple state)
 {
     Next_State_str_vector distribution_list;
     Player attacker;
     Player victim;
+    State_tuple distribution_tuple;
     short attacker_index = get<2>(state);
 
     if (attacker_index == 0)
@@ -27,20 +49,15 @@ Next_State_str_vector distribute(State_tuple state)
 
     // in distribution, attacker is the one who gets changed.
     // call _distribution_pair, do some operation and return
+    vector<Player> dp = distribution_pairs(attacker);
+
+    for (short i = 0; i < dp.size(); i++)
+    {
+        get<0>(distribution_tuple) = dp[i];
+        get<1>(distribution_tuple) = victim;
+        get<2>(distribution_tuple) = get<2>(distribution_tuple);
+        distribution_list.push_back(tuple_to_str(distribution_tuple));
+    }
 
     return distribution_list;
-}
-
-static vector<tuple<short, short>> distribution_pairs(tuple<short, short> fingers)
-{
-    vector<tuple<short, short>> distribution_pairs_list;
-    //some code here
-    auto sum = static_cast<short>(get<0>(fingers)+get<1>(fingers));
-    for (short i = 0; i < MAX_FINGER; i++)
-    {
-        tuple<short, short> sol_i;
-        get<0>(sol_i) = min(i, sum-get);
-
-    }
-    return distribution_pairs_list;
 }
